8d681706e6b45406aee64d7c449c482eea1a811b
Christian <christian@schladetsch.com>
2021-05-27 22:13:53 +1000

    Add SDL2 support for windows and pi.

--- ChessClock-SDL/Readme.md
+++ ChessClock-SDL/Readme.md
@@ -1,8 +1,29 @@
-# ChessClock-SDL
+SDL2, C++17 and CMake
+=====================
 
-Implementation based on SDL2.
+Requirements
+------------
 
-## Required components
+* CMake
+* Compiler that supports C++17 (recent version of `g++` or `clang++`)
+* SDL2
+* ninja (or `make`, just drop the `-GNinja` flag and build with `make`)
 
-* SDL_Font
+One way of building with C++17, SDL2, CMake and Ninja
+-----------------------------------------------------
 
+    mkdir -p build
+    cd build
+    cmake -GNinja ..
+    ninja
+    cd ..
+
+Running
+-------
+
+    build/main
+
+Cleaning up the binary file and build directory
+-----------------------------------------------
+
+    rm -rf build/
---
+++ ChessClock-SDL/Source/main.cpp
@@ -0,0 +1,66 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <cstdlib>
+#include <iostream>
+
+#define SDL_MAIN
+
+int main()
+{
+    using std::cerr;
+    using std::endl;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        cerr << "SDL_Init Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
+    if (win == nullptr) {
+        cerr << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer* ren
+        = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == nullptr) {
+        cerr << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Surface* bmp = SDL_LoadBMP("sample.bmp");
+    if (bmp == nullptr) {
+        cerr << "SDL_LoadBMP Error: " << SDL_GetError() << endl;
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+    if (tex == nullptr) {
+        cerr << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << endl;
+        SDL_FreeSurface(bmp);
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+    SDL_FreeSurface(bmp);
+
+    for (int i = 0; i < 20; i++) {
+        SDL_RenderClear(ren);
+        SDL_RenderCopy(ren, tex, nullptr, nullptr);
+        SDL_RenderPresent(ren);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
