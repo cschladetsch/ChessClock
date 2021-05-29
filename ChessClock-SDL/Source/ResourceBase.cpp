08a799d2e24aca522de78d034fa434ec61069c3e
Christian <christian@schladetsch.com>
2021-05-29 16:56:08 +1000

    Add nhlowmann::json submodule.

--- ChessClock-SDL/Source/ResourceBase.cpp
+++ ChessClock-SDL/Source/ResourceBase.cpp
@@ -1,9 +1,11 @@
 #include "ChessClock/ResourceBase.hpp"
 
+#include "nlohmann/json.hpp"
+
 namespace ChessClock
 {
     ResourceBase::ResourceBase(int id)
     {
-
+        nlohmann::json js;
     }
 }
