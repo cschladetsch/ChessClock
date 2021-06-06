5dd5531ac3068ac8cb9a04eca4f09d39f382c397
Christian <christian@schladetsch.com>
2021-06-07 01:26:01 +1000

    Add callbacks from scene.

--- ChessClock/Include/ChessClock/Game.hpp
+++ ChessClock/Include/ChessClock/Game.hpp
@@ -1,12 +1,41 @@
 #pragma once
 
+#include <functional>
 #include "ChessClock/Player.hpp"
 
 namespace ChessClock
 {
-    class Game
+    using string = std::string;
+
+    class UiCallBacks
     {
     public:
+        typedef std::function<void()> Callback;
+
+    private:
+        typedef std::unordered_map<string, Callback> Callbacks;
+        Callbacks _callbacks;
+
+    public:
+        void AddCallback(string name, Callback);
+        void RemoveCallback(string name);
+    };
+
+    class Game
+        : public UiCallBacks
+    {
         Player playerLeft, playerRight;
+
+    public:
+        void PressedLeft();
+        void PressedRight();
+
+        void PressedOptions();
+        void PressedPause();
+        void PressedSetup();
+        void PressedSound();
+
+    private:
+        void RegisterCallbacks();
     };
 }
---
+++ ChessClock/Source/Game.cpp
@@ -0,0 +1,32 @@
+#include "ChessClock/Game.hpp"
+
+namespace ChessClock
+{
+    void Game::RegisterCallbacks()
+    {
+    }
+
+    void Game::PressedLeft()
+    {
+    }
+
+    void Game::PressedRight()
+    {
+    }
+
+    void Game::PressedOptions()
+    {
+    }
+
+    void Game::PressedPause()
+    {
+    }
+
+    void Game::PressedSetup()
+    {
+    }
+
+    void Game::PressedSound()
+    {
+    }
+}
