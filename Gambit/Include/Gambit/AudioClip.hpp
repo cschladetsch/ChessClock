6764739c1906126feb29ace103a4ac475a60b184
Christian <christian@schladetsch.com>
2021-06-14 08:49:02 +1000

    Adding AudioClip.

--- Gambit/Include/Gambit/AudioClip.hpp
+++ Gambit/Include/Gambit/AudioClip.hpp
@@ -2,6 +2,7 @@
 
 #include "Gambit/Logger.hpp"
 #include "Gambit/Resource.hpp"
+#include "Gambit/ThirdParty/SDL.hpp"
 
 namespace Gambit
 {
@@ -10,13 +11,16 @@ namespace Gambit
     {
         static inline Logger _log{ "AudioClip" };
 
+        SDL_AudioCVT cvt; 
+
     public:
         AudioClip(ResourceId const &id, const char *file);
-
-        void Deleter(AudioClip *);
 
         void Play();
         void SetVolume(float zeroToOne);
         void Stop();
+
+    protected:
+        void DeleteRaw(uint8_t *ptr);
     };
 }
--- Gambit/Source/AudioClip.cpp
+++ Gambit/Source/AudioClip.cpp
@@ -1,13 +1,35 @@
 #include "Gambit/AudioClip.hpp"
+#include "Gambit/ThirdParty/SDL.hpp"
 
 namespace Gambit
 {
     AudioClip::AudioClip(ResourceId const &id, const char *file)
         : ResourceBase(id)
     {
+        int index{ 0 };
+        SDL_AudioSpec wave;
+        Uint8 *data{ 0 };
+        Uint32 dlen{ 0 };
+        if (SDL_LoadWAV(file, &wave, &data, &dlen) == NULL)
+        {
+            LOG_ERROR() << "Couldn't load " << file << " as .wav file";
+            return;
+        }
+        SDL_BuildAudioCVT(&cvt, wave.format, wave.channels, wave.freq, AUDIO_S16, 2, 22050);
+        cvt.buf = (uint8_t *)malloc(dlen * cvt.len_mult);
+        if (cvt.buf == 0)
+        {
+            LOG_ERROR() << "Couldn't load " << file << " as .wav file";
+            return;
+        }
+
+        memcpy(cvt.buf, data, dlen);
+        cvt.len = dlen;
+        SDL_ConvertAudio(&cvt);
+        SDL_FreeWAV(data);
     }
 
-    void AudioClip::Deleter(AudioClip *)
+    void AudioClip::DeleteRaw(uint8_t *ptr)
     {
     }
 
